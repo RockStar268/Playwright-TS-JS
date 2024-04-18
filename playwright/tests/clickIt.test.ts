bdb57c207bbd2bed7f17657503bff91e22bc5112
Rocky Kuang <shrkuang@gmail.com>
2024-04-18 09:36:36 +0200

    scope:  - remove duplicate code in clickIt.test.ts

--- playwright/tests/clickIt.test.ts
+++ playwright/tests/clickIt.test.ts
@@ -1,19 +1,28 @@
 import { test , expect } from '../resources/config/fixtures';
 import { levelUpMessage, LeveledUpParagraphText } from '../resources/enums/levelUpMessages';
 
-
-const charName = ' 123 Test ';
 const build = 'Mage'
 
+test.beforeEach(async ({ page, homepage, playpage }) => {
+    const charName = ' 123 Test ';
 
-test("Click 5 times to level up", async ({ page, homepage, playpage, levelup}) =>{
-
+    // Runs before each test and signs in each page.
     await page.goto("/");
     await homepage.clickOnPlayButton();  
 
     await playpage.selectBuild(build);
     await playpage.fillInCharacterName(charName);
     await playpage.clickStartButton();
+  });
+
+test("Click 5 times to level up", async ({ page, homepage, playpage, levelup}) =>{
+
+    // await page.goto("/");
+    // await homepage.clickOnPlayButton();  
+
+    // await playpage.selectBuild(build);
+    // await playpage.fillInCharacterName(charName);
+    // await playpage.clickStartButton();
 
     await levelup.clickButtonToLevelUp(5);
     await expect(levelup.clickItButton).toBeDisabled();
@@ -25,12 +34,12 @@ test("Click 5 times to level up", async ({ page, homepage, playpage, levelup}) =
 
 
 test("Click 4 times and no level up", async ({ page, homepage, playpage, levelup}) =>{
-    await page.goto("/");
-    await homepage.clickOnPlayButton();  
+    // await page.goto("/");
+    // await homepage.clickOnPlayButton();  
 
-    await playpage.selectBuild(build);
-    await playpage.fillInCharacterName(charName);
-    await playpage.clickStartButton();
+    // await playpage.selectBuild(build);
+    // await playpage.fillInCharacterName(charName);
+    // await playpage.clickStartButton();
 
     await levelup.clickButtonToLevelUp(4);
     await expect(levelup.clickItButton).toBeEnabled();
@@ -42,11 +51,11 @@ test("Click 4 times and no level up", async ({ page, homepage, playpage, levelup
 
 
 test("Click 6 times, out of bound", async ({ page, homepage, playpage, levelup}) =>{
-    await page.goto("/");
-    await homepage.clickOnPlayButton();  
-    await playpage.selectBuild(build);
-    await playpage.fillInCharacterName(charName);
-    await playpage.clickStartButton();
+    // await page.goto("/");
+    // await homepage.clickOnPlayButton();  
+    // await playpage.selectBuild(build);
+    // await playpage.fillInCharacterName(charName);
+    // await playpage.clickStartButton();
 
     const clickFunction = async () => {
         await levelup.clickButtonToLevelUp(6);
